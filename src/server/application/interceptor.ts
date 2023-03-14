c1c62691fa31f6c4d2e2626c411080680898b5d0
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-14 09:57:00 +0100

    Add logged interceptor

---
+++ src/server/application/interceptor.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import { Auth, TokenPayload } from './auth.js';
+import { HTTPError } from '../../common/error.js';
+
+export interface RequestCool extends Request {
+  info?: TokenPayload;
+}
+
+export class Interceptors {
+  constructor() {}
+
+  logged(req: RequestCool, resp: Response, next: NextFunction) {
+    const authHeader = req.get('Authorization');
+
+    try {
+      if (!authHeader)
+        throw new HTTPError(498, 'Token expired', 'No value in http header');
+
+      if (!authHeader.startsWith('Bearer'))
+        throw new HTTPError(498, 'Token invalid', 'Not Bearer in auth header');
+
+      const token = authHeader.slice(7);
+      const payload = Auth.verifyJWT(token);
+      req.info = payload;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
