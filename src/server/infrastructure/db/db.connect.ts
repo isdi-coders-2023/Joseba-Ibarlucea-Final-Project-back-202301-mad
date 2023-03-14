f747726f76bba2542a1bdab64582bbe7d48818bc
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-14 09:55:41 +0100

    Add port debug

--- src/server/infrastructure/db/db.connect.ts
+++ src/server/infrastructure/db/db.connect.ts
@@ -2,14 +2,14 @@ import { config } from '../../../config.js';
 import mongoose from 'mongoose';
 import createDebug from 'debug';
 
-const debug = createDebug('Fntic: dnConnect');
+const debug = createDebug('Fntic: dbConnect');
 
 export const dbConnect = (env?: string) => {
   const finalEnv = env || process.env.NODE_ENV;
   const finalDBname =
     finalEnv === 'test' ? config.dbName + '_Testing' : config.dbName;
 
-  const uri = `mongodb+srv://${config.user}:${config.passwd}@${config.cluster}/${config.dbName}?retryWrites=true&w=majority`;
-  debug(uri);
+  const uri = `mongodb+srv://${config.user}:${config.passwd}@${config.cluster}/${finalDBname}?retryWrites=true&w=majority`;
+  debug(config.dbName);
   return mongoose.connect(uri);
 };
